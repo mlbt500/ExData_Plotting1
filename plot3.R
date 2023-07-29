502d7358f5b36a68623b015db867a2c89410bcf5
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-07-29 15:27:55 +0100

    plot 3

---
+++ plot3.R
@@ -0,0 +1,6 @@
+with(data2,{
+  plot(Sub_metering_1~DateTime, col = "black", type = "l", ylab = "Sub meter reading", xlab = "")
+  lines(Sub_metering_2 ~ DateTime, col = "blue")
+  lines(Sub_metering_3 ~ DateTime, col = "red")
+})
+legend("topright", legend = c("meter 1", "meter 2", "meter 3"), fill = c("black", "blue", "red"))
