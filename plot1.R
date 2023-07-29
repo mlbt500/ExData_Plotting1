4efb314bf8fd00cd78a2007f053a7335299842e0
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-07-29 14:36:12 +0100

    Plot 1 and plot 2 code. Not added PNG yet.

---
+++ plot1.R
@@ -0,0 +1,3 @@
+##plot graph
+hist(data2$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kw)", col = "red")
+##create PNG file
---
+++ plot2.R
@@ -0,0 +1,3 @@
+##plot graph
+plot(Global_active_power~DateTime, data = data2, type = "l", ylab = "Global active power (kw)", xlab = "")
+## create PNG file
